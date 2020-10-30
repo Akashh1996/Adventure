fce585bdf2785905b4e2cf737ea781087668a9ae
Gerard Ramon <gerardramonp@gmail.com>
2020-10-30 12:42:26 +0100

    first changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ function App() {
 				src="https://trello-attachments.s3.amazonaws.com/5f9bfa5dd498fc35d70d6d4f/1199x314/5f40bb3b41e9e071b43776f5d90ab72d/71JB6lAf3uL._AC_SL1500_.jpg"
 				alt="akash-47"
 			></img>
+			<div>
+				<img
+					src="https://ca.slack-edge.com/T0SJKHBFZ-U016XU900E6-fa0eb758bde3-512"
+					alt="animal-abandonado"
+				></img>
+				<img
+					src="https://ca.slack-edge.com/T0SJKHBFZ-U016VE1N4PN-3b8b61cef136-512"
+					alt="eric-s"
+				></img>
+			</div>
 		</div>
 	);
 }
