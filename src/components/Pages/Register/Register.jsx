08508ec5b73b261568c39d7cf38379c286c7ddc0
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2020-10-31 22:32:55 +0100

    changed colors & register form init

---
+++ src/components/Pages/Register/Register.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './Register.css';
+
+class Form extends Component {
+	render() {
+		return (
+			<div className="wrapper">
+				<div className="form-wrapper">
+					<h1>Create Account</h1>
+					<form onSubmit={this.handleSubmit} noValidate>
+						<div className="firstName">
+							<label htmlFor="firstName">First Name</label>
+							<input
+								type="text"
+								className="first-name"
+								placeholder="First Name"
+								name="firstName"
+								onChange={this.handleChange}
+								noValidate
+							/>
+						</div>
+						<div className="lastName">
+							<label htmlFor="lastName">Last Name</label>
+							<input
+								type="text"
+								className="last-name"
+								placeholder="Last Name"
+								name="lastName"
+								onChange={this.handleChange}
+								noValidate
+							/>
+						</div>
+						<div className="email">
+							<label htmlFor="email">Email</label>
+							<input
+								type="text"
+								className="user-email"
+								placeholder="Email"
+								name="email"
+								onChange={this.handleChange}
+								noValidate
+							/>
+						</div>
+						<div className="password">
+							<label htmlFor="password">Password</label>
+							<input
+								type="text"
+								className="user-password"
+								placeholder="Password"
+								name="password"
+								onChange={this.handleChange}
+								noValidate
+							/>
+						</div>
+					</form>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Form;
