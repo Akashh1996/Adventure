0eef6f421f24dc6f53c999022d1dd0f63a5ba325
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2020-11-02 11:38:11 +0100

    all changes done

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import NavBar from './components/Header/NavBar';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/Pages/Login/Login';
 import Register from './components/Pages/Register/Register';
+import Footer from './components/Footer/Footer';
+import Help from './components/Pages/Help/Help';
+import Contact from './components/Pages/Contact/Contact';
+import Slider from './components/Pages/Home/Slider';
+import MapContainer from './components/Pages/Map/Map';
 
 function App() {
-
 	return (
 		<>
 			<BrowserRouter>
@@ -13,7 +17,12 @@ function App() {
 				<Switch>
 					<Route path="/register" component={Register} />
 					<Route path="/Login" component={Login} />
+					<Route path="/Help" component={Help} />
+					<Route path="/Contact" exact component={Contact} />
+					<Route path="/" exact component={Slider} />
+					<Route path="/maps" component={MapContainer} />
 				</Switch>
+				<Footer />
 			</BrowserRouter>
 		</>
 	);
