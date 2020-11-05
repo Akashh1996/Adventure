5bc38a7252de3e4aa37a4e9deabc57dafbd804de
Alma-Co <edith.cantegrit@gmail.com>
2020-11-05 19:27:26 +0100

    myprofile json, store and actions update

--- src/components/Pages/Profile/Profile.jsx
+++ src/components/Pages/Profile/Profile.jsx
@@ -1,34 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import userStore from '../../../store/user-store';
-import { loadUser } from '../../../actions/place-actions';
+import { loadMyProfile } from '../../../actions/place-actions';
 import './Profile.css';
 
 function Profile() {
-	const [user, setUser] = useState(userStore.getUser());
-	const [favorites, setFavorites] = useState(userStore.getMyFavorites());
+	const [profile, setProfile] = useState(userStore.getMyProfile());
 
 	function handleChange() {
-		setUser(userStore.getUser());
-		setFavorites(userStore.getMyFavorites());
+		setProfile(userStore.getMyProfile());
 	}
 
 	useEffect(() => {
 		userStore.addEventListener(handleChange);
 
-		if (!user) {
-			loadUser();
+		if (!profile) {
+			loadMyProfile();
 		}
 
 		return () => {
 			userStore.removeEventListener(handleChange);
 		};
-	}, [user, favorites]);
+	}, [profile]);
 
 	return (
 		<>
-			{/* HEADER */}
-			{user && (
+			{profile && (
 				<>
 					<header className="">
 						<Link to="/home">
@@ -39,23 +36,25 @@ function Profile() {
 					<section>
 						<div class="userProfile">
 							<span className="userProfile--picture">
-								<img src={user.picture} alt="" />
+								<img src={profile[0]['me']['my_picture']} alt="" />
 							</span>
-							<h2 className="userProfile--name">{user.name}</h2>
+							<h2 className="userProfile--name">
+								{profile[0]['me']['my_name']}
+							</h2>
 						</div>
 					</section>
 					{/* FAVOURITES */}
+
 					<section>
 						{/* FAVOURITES HEADER */}
 						<div>
 							<h3>Favoritos</h3>
 							<button>Edit my favourites</button>
 						</div>
+
 						{/* FAVOURITES CARD DETAIL */}
-						<div className="card-deck">
-							{/* .map del objeto favourites / create bootstrap card / div flex
-							horizontal */}
-							{favorites.map((FavoriteDetail) => {
+						<div className="cardFav">
+							{profile[0]['my_favourites'].map((FavoriteDetail) => {
 								return (
 									<div className="card">
 										<figure>
@@ -75,6 +74,7 @@ function Profile() {
 							})}
 						</div>
 					</section>
+
 					{/* PICTURES */}
 					<section>
 						{/* PICTURES HEADER */}
@@ -85,20 +85,48 @@ function Profile() {
 						<div>slider with pictures</div>
 					</section>
 					{/* REVIEWS */}
+
 					<section>
 						<div>
 							<h3>Mis Reviews</h3>
-							<button>Buscar reviews</button>
+							<button>Edit my reviews</button>
 						</div>
-						<div>slider with pictures</div>
+						<div className="reviewsCard">
+							{profile[0]['my_reviews'].map((ReviewDetail) => {
+								return (
+									<div>
+										<h1>{ReviewDetail.place_name}</h1>
+										<h2>{ReviewDetail.my_rating}</h2>
+										<h3>{ReviewDetail.url}</h3>
+										<text>{ReviewDetail.my_description}</text>
+										<h4>{ReviewDetail.user_name}</h4>
+									</div>
+								);
+							})}
+						</div>
 					</section>
+
 					{/* FRIENDS */}
 					<section>
 						<div>
 							<h3>Mi círculo de amigos</h3>
 							<button>Invitar a un amigo</button>
 						</div>
-						<div>.map de fotos de usuario que crea un span con una imagen.</div>
+						<div className="cardFav">
+							{profile[0]['my_friends'].map((FriendsDetail) => {
+								return (
+									<div className="card">
+										<figure>
+											<img
+												src={FriendsDetail['friend_picture']}
+												alt="PictBestFriend"
+											/>
+										</figure>
+										<h3>{FriendsDetail.name}</h3>
+									</div>
+								);
+							})}
+						</div>
 					</section>
 				</>
 			)}
